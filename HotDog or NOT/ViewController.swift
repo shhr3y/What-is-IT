a078b5b90ad6f94afe8a73c276a51434389dc649
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-07-02 11:50:17 +0530

    Initial Commit

---
+++ HotDog or NOT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDog or NOT
+//
+//  Created by Shrey on 02/07/20.
+//  Copyright © 2020 Shrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
