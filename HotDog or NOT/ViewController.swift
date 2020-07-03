ee6930d107b7060bdb03a359bc61e47943192b36
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-07-03 07:18:51 +0530

    Completed What is it.

--- HotDog or NOT/ViewController.swift
+++ HotDog or NOT/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
     }
 
-
+    @IBAction func CameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            print("utha lia pic")
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not Convert UIImage to CIImage")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed!")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model Failed to process Image.")
+            }
+            if let object = result.first{
+                self.navigationItem.title = "\(object.identifier) \(String(format: "%.2f", object.confidence*100))"
+            }
+        }
+        
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+    
 }
 
